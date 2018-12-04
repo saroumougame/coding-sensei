533a08020f3022452462712d97dfd58cc6faf32d
Aroumougame <tokuro@hotmail.fr>
2018-12-04 16:33:11 +0100

    correction mapping

--- src/Entity/Exercice.php
+++ src/Entity/Exercice.php
@@ -10,7 +10,6 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\ExerciceRepository")
- * @ORM\Table(name="exercice")
  */
 class Exercice
 {

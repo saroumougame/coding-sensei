60fdc95b6df84428d961223bd4a8bf8a9dd7188f
nico <n.tiesenhausen@gmail.com>
2019-06-24 22:01:30 +0200

    supr tout les inutile pour le front

--- docker2/asset/asset.values.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-    output_directory: 'public',
-    container_name: 'asset_adboghh30jp9s2ij4',
-};
--- docker2/composer/composer.values.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-    application_dir: './',
-    container_name: 'composer_adboghh30jp9s2ij5',
-};

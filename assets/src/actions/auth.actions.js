869faeeb4a5f787b633aa3f4a788267f5f6c0a1b
Nicolas <n.tiesenhausen@gmail.com>
2019-06-26 11:44:56 +0200

    test fix travis + api

--- assets/src/actions/auth.actions.js
+++ assets/src/actions/auth.actions.js
@@ -19,7 +19,7 @@ export const LOGIN_FAIL              = "LOGIN_FAIL";
 export const EXPIRED                 = "EXPIRED";
 export const GET_USER                 = "GET_USER";
 
-       const API_URL                  = 'http://51.38.38.246:8080';
+       const API_URL                  = 'http://api.coding-sensei.fr';
 
 export const inscriptionEtapeAction = etape => ({
   type: INSCRIPTION_ETAPES,

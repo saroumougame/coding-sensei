fe90dafd5bf90a3e743ec3f3ff1064277e6a9766
nico <n.tiesenhausen@gmail.com>
2019-06-30 22:17:09 +0200

    Exercice list + eleve un peut

--- assets/src/containers/exercice/ListeMesExercices/contact-details/contact-details.component.js
+++ assets/src/containers/exercice/ListeMesExercices/contact-details/contact-details.component.js
@@ -7,11 +7,12 @@ import themeStyles from './contact-details.theme.style';
 import withWidth from '@material-ui/core/withWidth';
 import { withStyles } from '@material-ui/core/styles';
 import scss from './contact-details.module.scss';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import {FormUpdateClassNom, updateClass, deleteClass}      from '../../../../actions/classes.actions';
 
 
 import Eleve   from '../../../eleves/eleve/contacts.component';
-
 
 class ContactDetails extends React.Component {
 
@@ -20,159 +21,38 @@ class ContactDetails extends React.Component {
 
     this.state = {
       nom: '',
-
     }
-  }
-
-  handleNameUpdate = (e) => {
-    this.props.FormUpdateClassNom(e.target.value);
-  }
-
-  formSubmit() {
-      if(this.props.data.FormDataUpdateClassNom !== ''){
-        this.props.onSelect(null);
-        this.props.updateClass(this.props.data.FormDataUpdateClassNom, this.props.selectedContact.id);    
-      }
-      return;
   }
 
   render(){
   const {
-    classes
+    classes,
+    selectedContact
   } = this.props;
+ 
+  console.log(selectedContact);
 
   return (
-    <div className={classNames(scss['portal-contact-details'] )}>
-      {/*<div
-        className={classNames(
-          scss['portal-contact-details__header'],
-
-        )}
-      >
-      <div className={classNames(scss['class-detail-header'])}>
-        <Paper className={scss['class-name-paper']} elevation={4}>
-          <Typography variant="headline" component="h3">
-            {selectedContact.name}
-          </Typography>
+    <div className={classNames(scss['portal-contact-details'])}>
+      <div className={scss['exercice-title']}>
+        <Paper className={scss['paper-description']}>
+           <Typography component="h3">Titre: {selectedContact.name}</Typography>
         </Paper>
-        <Paper className={scss['class-name-paper-central']} elevation={4}>
-          <Typography variant="headline" component="h3">
-            Information
-                              </Typography>
-
-               <form  noValidate autoComplete="off">
-                  <TextField
-                    id="nom"
-                    value={this.props.data.FormDataUpdateClassNom}
-                    onChange={this.handleNameUpdate}
-                    label="Nom de la classe"
-                    className={classes.textField}
-                    margin="normal"
-                  />
-                <Button variant="contained" color="primary" className={scss['portal-contacts-detail-update-contact']} onClick={() => {this.formSubmit()}}>
-                    Modifier la classe
-                  </Button>
-                </form>      
-</Paper>
-                              <Button variant="contained" color="primary" className={scss['portal-contacts-detail-delete-button-contact']} onClick={() => {this.props.deleteClass(selectedContact.id)}}>
-
-                    Supprimer la classe
-                </Button>
+        <Paper className={scss['paper-description']}>
+          <Typography component="h3">Description: {selectedContact.description}</Typography>
+        </Paper>      
+        <Paper className={scss['paper-description']}>
+            <Typography component="h3">Donnée en entrée: {selectedContact.inData}</Typography>
+        </Paper> 
+        <Paper className={scss['paper-description']}>
+            <Typography component="h3">Donnée attendu en sortie: {selectedContact.outData}</Typography>
+        </Paper> 
+         <Paper className={scss['paper-description']}>
+            <Typography component="h3">Date limite: {selectedContact.dateEnd}</Typography>
+        </Paper>        
+        
       </div>
-
-
-      </div>*/}
-      <div
-        className={classNames(
-          scss['portal-contact-details__content'],
-          classes.portalContactDetailsContent
-        )}
-      >
-   
-    {/*
-
-<Grid container className={classes.root} spacing={16}>
-        <Grid item xs={12}>
-          <Grid container className={classes.demo} justify="center" spacing={32}>
-          
-              <Grid  item>
-                <Paper className={classNames(scss['portal-contact-paper-grid'] )}>
-                Liste des exercices
-                </Paper>
-              </Grid>
-              <Grid  item>
-               <Paper className={classNames(scss['portal-contact-paper-grid'] )}>
-                Liste des Eleves
-              
-                </Paper>
-              </Grid>
-              <Grid  item>
-                <Paper className={classNames(scss['portal-contact-paper-grid'] )}>
-                  Mes notes
-                </Paper>
-              </Grid>
-              <Grid  item>
-                <Paper className={classNames(scss['portal-contact-paper-grid'] )}>
-                  Gestion
-                </Paper>
-              </Grid>         
-          </Grid>
-        </Grid>
- </Grid>
- */}
-      {/*
-        <Grid container spacing={0}>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <div
-              className={classNames(
-                scss['portal-contact-details__avatar-container']
-              )}
-            > 
-
-
-              <div
-                className={classNames(
-                  scss['portal-contact-details__avatar'],
-                  classes.portalContactDetailsAvatar
-                )}
-              >
-                <img
-                  className={classNames(
-                    classes.portalContactDetailsAvatarImg
-                  )}
-                  src={`${process.env.PUBLIC_URL}/${selectedContact.photo}`}
-                  alt={`${process.env.PUBLIC_URL}/${selectedContact.name}`}
-                />
-              </div>
-              <div
-                className={classNames(
-                  scss['portal-contact-details__extra'],
-                  classes.portalContactDetailsExtra
-                )}
-              >
-                <p>{selectedContact.region}</p>
-                <p>{selectedContact.phone}</p>
-                <p>{selectedContact.email}</p>
-                <br />
-
-  
-              </div>
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6} md={9}>
-            <div
-              className={classNames(
-                scss['portal-contact-details__main']
-              )}
-            >
-
-              <Typography component="div" dangerouslySetInnerHTML={{ __html: selectedContact.bio }} />
-            </div>
-          </Grid>
-        </Grid>
-            */}
-      </div>
+      
     </div>
   );
 }

7095bf49e212949bdf7a985b54cdb337df51cf46
nico <n.tiesenhausen@gmail.com>
2019-06-30 14:57:54 +0200

    commit avancée

--- assets/src/containers/eleves/listeCours/ListeCours.component.js
+++ assets/src/containers/eleves/listeCours/ListeCours.component.js
@@ -22,8 +22,12 @@ class ListeCours extends React.Component {
    getDateLimit(exo) {
     if(exo.dateEnd == null) {
       return 'Pas de date limite';
-    }
+    } 
+
+      return 'Disponible le : '+this.parseDate(exo.dateEnd);
+    //console.log(exo.dateEnd);
    }
+
 
    parseDate(uneDate) {
     if(uneDate != null){
@@ -36,18 +40,29 @@ class ListeCours extends React.Component {
    }
 
    getListeExercices() {
+    // si pas d'exerice
     if(typeof(this.props.data.liste_exercice_user) == undefined || typeof(this.props.data.liste_exercice_user) == 'undefined'){
       return;
     }
 
+        // on loop sur les exo
        return this.props.data.liste_exercice_user.map((i) => {
 
           if(i.exercice.archive === true) {
             return;
           }
-          console.log(i.reponse);
-          if(typeof(i.reponse[0]) != 'undefined' && typeof(i.reponse[0]) != undefined){
-            if(i.reponse[0].success == true) {
+
+          //on loop sur les reponses 
+          var BonneReponse = false;
+          i.reponse.map((i) =>{
+            console.log(i);
+            if(i.success == true) {
+              BonneReponse  = true;
+            } 
+          });
+
+          // si une bonne reponse
+          if(BonneReponse == true){
               return (
                       <div key={i.exercice.name} className={scss['un-cour-valider']} onClick={() => this.showExercice(i)} >
                           <div>
@@ -58,18 +73,24 @@ class ListeCours extends React.Component {
                          </div>
                       </div>
               );
-            }
           }
+
+          //if()
+          //if(this.datePassedExercice(i.exercice))
+          // si la date n'est pas encore passée 
           return (
+
             <div key={i.exercice.name} className={scss['un-cour']} onClick={() => this.showExercice(i)} >
                 <div>
                 {i.exercice.name}
                 </div>
-               <div>
-                  {this.getDateLimit(i.exercice)}
-                <div>
-                         nombre de tentatives :{this.getTentatives(i.reponse)}
-                </div>
+               <div className={scss['date-tentatives']}>
+                  <div>
+                   {this.getDateLimit(i.exercice)}
+                   </div>
+                   <div>
+                    nombre de tentatives :{this.getTentatives(i.reponse)}
+                   </div>
                </div>
 
 
